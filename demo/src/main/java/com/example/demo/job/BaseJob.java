c24540a8ca1d0eca83238eb878eda5e6e6b50bec
niuhaijun <nhjemail@163.com>
2019-03-02 12:09:11 +0800

    格式化代码

--- demo/src/main/java/com/example/demo/job/BaseJob.java
+++ demo/src/main/java/com/example/demo/job/BaseJob.java
@@ -4,7 +4,8 @@ import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
-public interface BaseJob extends Job{
-	public void execute(JobExecutionContext context) throws JobExecutionException;
+public interface BaseJob extends Job {
+
+  void execute(JobExecutionContext context) throws JobExecutionException;
 }
 
